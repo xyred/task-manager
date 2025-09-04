a26a114ac7c0d7d26a0d92e7f43c457ef4f422f7
xyred <felixherold@gmx.de>
2025-09-04 11:57:53 +0200

    feat: enable CORS for TaskController
    - Add @CrossOrigin annotation to allow requests from http://localhost:5173

--- src/main/java/de/fherold/task_manager/controller/TaskController.java
+++ src/main/java/de/fherold/task_manager/controller/TaskController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import org.springframework.web.bind.annotation.PutMapping;
  * Provides endpoints for task operations.
  */
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 public class TaskController {
 
