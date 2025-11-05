48ae282a23b640684da352fd196d747bb75d9ee1
xyred <felixherold@gmx.de>
2025-11-05 16:11:24 +0100

    feat: Remove CORS properties and update CORS configuration to allow requests from specified origins

--- src/main/java/de/fherold/task_manager/config/CorsConfig.java
+++ src/main/java/de/fherold/task_manager/config/CorsConfig.java
@@ -1,7 +1,5 @@
 package de.fherold.task_manager.config;
 
-import java.util.List;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.lang.NonNull;
@@ -10,30 +8,22 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * Configuration class to set up CORS (Cross-Origin Resource Sharing) settings.
- * It allows requests from specified origins to access the /tasks/** endpoints.
+ * It allows requests from specified origins to access the /api/** endpoints.
  */
 
 @Configuration
 public class CorsConfig {
-
-    private final CorsProperties corsProperties;
-
-    public CorsConfig(CorsProperties corsProperties) {
-        this.corsProperties = corsProperties;
-    }
 
     @Bean
     WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(@NonNull CorsRegistry registry) {
-                List<String> origins = corsProperties.getAllowedOrigins();
-                if (origins == null || origins.isEmpty()) {
-                    origins = List.of("http://localhost:5173");
-                }
-                registry.addMapping("/tasks/**")
-                        .allowedOrigins(corsProperties.getAllowedOrigins().toArray(new String[0]))
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                                "http://localhost:5173",
+                                "https://task-manager-frontend-plum-five.vercel.app/")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*");
             }
         };
